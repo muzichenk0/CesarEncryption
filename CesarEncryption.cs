8d99842d9bbdc3d9490239c60c6a9b1f1be54dab
season <onionmarker@gmail.com>
2022-11-20 23:13:02 +0300

    added latin alphabet

--- CesarEncryption.cs
+++ CesarEncryption.cs
@@ -1,16 +1,15 @@
-﻿
-using System.Runtime.ExceptionServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Text.RegularExpressions;
 using System.Text;
 
 
 Console.Write("Type text: ");
 string ChoosenText = Console.ReadLine();
 
+
 Console.WriteLine("What do u want to do?\n 1 - Encrypt text\n 2 - DeCrypt text");
 int vibor = Int32.Parse(Console.ReadLine());
 
-ReadyComplect ToWork = new ReadyComplect("rus", ChoosenText);
+ReadyComplect ToWork = new ReadyComplect(ChoosenText);
 
 CesarEncryption FirsAttempt = new CesarEncryption(ToWork, vibor);
 
@@ -19,27 +18,40 @@ class ReadyComplect
     public string? Alphavet { get; set; }
     public string? TextToCrypt { get; set; }
 
-    public string[] Alphabets = new string[2] { "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛИНОПРСТУФХЦЧШЩЬЫЬЭЮЯ", ""};
-    public ReadyComplect(string alpha, string text)
+    private string Kirillica = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛИНОПРСТУФХЦЧШЩЬЫЬЭЮЯ";
+    private string Latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    public ReadyComplect(string text)
     {
-        if (alpha == "rus")
-        {
-            Alphavet = Alphabets[1];
-        }
-        else if (alpha == "eng")
-        {
-            Alphavet = Alphabets[2];
-        }
+       
+            Regex russian = new Regex("абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛИНОПРСТУФХЦЧШЩЬЫЬЭЮЯ");
+            Match mr = russian.Match(text);
+
+            Regex english = new Regex("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+            Match me = english.Match(text);
+            if (mr.Success)
+            {
+            Alphavet = Kirillica;
+            }
+            else if (me.Success)
+            {
+            Alphavet = Latin;
+            }
+            else
+            {
+                Alphavet = Kirillica + Latin;
+            }
+
+       
+        
         TextToCrypt = text;
     }
 }
 
 class CesarEncryption : ReadyComplect
 {
-    private string Kirillica = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛИНОПРСТУФХЦЧШЩЬЫЬЭЮЯ";
-
-    public CesarEncryption(ReadyComplect nabor, int vibor) : base(nabor.Alphavet, nabor.Alphavet )
+    public CesarEncryption(ReadyComplect nabor, int vibor) : base(nabor.TextToCrypt)
     {
+
         switch (vibor)
         {
             case 1:
